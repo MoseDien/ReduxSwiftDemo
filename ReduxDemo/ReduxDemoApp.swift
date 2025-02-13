99d57be64a6c0c805fda708d1122001a6bc420dd
bell <dienbell@gmail.com>
2025-02-13 22:57:25 +0800

    Initial Commit

---
+++ ReduxDemo/ReduxDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReduxDemoApp.swift
+//  ReduxDemo
+//
+//  Created by Dien Bell on 2025/2/13.
+//
+
+import SwiftUI
+
+@main
+struct ReduxDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ReduxDemoTests/ReduxDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReduxDemoTests.swift
+//  ReduxDemoTests
+//
+//  Created by Dien Bell on 2025/2/13.
+//
+
+import Testing
+@testable import ReduxDemo
+
+struct ReduxDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
